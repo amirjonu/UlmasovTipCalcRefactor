7fc9b320ef39d9ce777d11b959d55695ff80b783
BT_3W2_13 <amirjonu@nycstudents.net>
2022-10-07 12:34:19 -0400

    as

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -1,2 +1,43 @@
 public class TipCalculator {
+    private int numPeople;
+    private int tipPercentage;
+    private double totalBillBeforeTip;
+
+    public TipCalculator(int numOfPeople, int tipPercentage){
+        numPeople= numOfPeople;
+        this.tipPercentage=tipPercentage;
+        totalBillBeforeTip= 0.0;
+    }
+
+    public double getTotalBillBeforeTip(){
+        return totalBillBeforeTip;
+
+    }
+
+    public int getTipPercentage(){
+        return tipPercentage;
+    }
+
+    public void addMeal(double cost){
+        totalBillBeforeTip += cost;
+    }
+
+    public double tipAmount(){
+        return (totalBillBeforeTip*((double)tipPercentage/100));
+    }
+
+    public double totalBill(){
+        return totalBillBeforeTip+tipAmount();
+    }
+    public double perPersonCostBeforeTip(){
+        return totalBillBeforeTip/numPeople;
+    }
+
+    public double perPersonTipAmount(){
+        return tipAmount()/numPeople;
+    }
+    public double perPersonTotalCost(){
+        return totalBill()/numPeople;
+    }
+
 }
--- src/TipCalculatorRunner.java
+++ src/TipCalculatorRunner.java
@@ -1,5 +1,30 @@
+
+import java.util.Scanner;
 public class TipCalculatorRunner {
     public static void main (String[] args){
-        
+        Scanner input= new Scanner(System.in);
+        System.out.println("-----------Welcome to the Tip Calculator-----------");
+
+        //Welcomes and asks for total bill and user inputs the total amount
+        System.out.print("Enter the total bill amount: $ ");
+        double totalBill= input.nextDouble();
+
+        //Asks for tip percentage and user enters it
+        System.out.print("Enter the tip percentage: %");
+        int tipPercentage= input.nextInt();
+
+        //Asks for total number of people and user enters it
+        System.out.print("Enter the total amount of people in your party: ");
+        int totalPeople= input.nextInt();
+
+        TipCalculator tipCalc= new TipCalculator(totalPeople, tipPercentage);
+        tipCalc.addMeal(totalBill);
+
+
+        System.out.println();
+        System.out.println("The total tip amount is: $"+ String.format("%.2f", tipCalc.tipAmount()));
+        System.out.println("The total amount of money including the bill and tip is: $"+ String.format("%.2f", tipCalc.totalBill()));
+        System.out.println("The tip amount per person is: $"+ String.format("%.2f", tipCalc.perPersonTipAmount()));
+        System.out.println("The total amount of money per person is: $"+ String.format("%.2f", tipCalc.perPersonTotalCost()));
     }
 }
